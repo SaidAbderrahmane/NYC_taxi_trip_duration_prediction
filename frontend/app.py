8e729f0b5f4442ccd534b368de2cc88522aa3df1
saidabder <saidabd3@gmail.com>
2024-12-03 15:54:15 +0100

    add streamlit app file

---
+++ frontend/app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import requests
+
+# Set up the title
+st.title("NYC Taxi Trip Prediction")
+
+# Get user input
+hour = st.slider("Hour", 0, 23, 12)
+weekday = st.slider("Weekday", 0, 6, 0)
+month = st.slider("Month", 1, 12, 1)
+
+# Make prediction button
+if st.button("Predict"):
+    # Send data to the FastAPI backend
+    response = requests.post(
+        "http://localhost:8000/predict",
+        json={"hour": hour, "weekday": weekday, "month": month}
+    )
+    
+    if response.status_code == 200:
+        predictions = response.json()
+        st.write("Prediction: ", predictions)
+    else:
+        st.write("Error: Unable to get prediction")
